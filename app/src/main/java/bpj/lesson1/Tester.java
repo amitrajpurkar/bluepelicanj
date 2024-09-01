6ada5570fd03d388ca643c51a6a57c28230015d5
Amit Rajpurkar <amit.rajpurkar@gmail.com>
2024-09-01 17:31:15 -0400

    adding a sample code file

---
+++ app/src/main/java/bpj/lesson1/Tester.java
@@ -0,0 +1,27 @@
+package bpj.lesson1;
+
+public class Tester {
+
+    // single line comment
+
+    /*
+     * multi-line comment
+     * this is line two
+     * */
+    public static void main(String args[]) {
+        System.out.println("Hello Work\n\n");
+
+        String s = """
+                From: Bill Smith
+                Address: Dell Computer, Bldg 13
+                Date: April 12, 2005
+
+                To: Jack Jones
+
+                Message: Help! I'm trapped inside a computer!
+                """;
+
+        System.out.println(s);
+    }
+
+}
